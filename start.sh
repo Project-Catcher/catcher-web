ca7a51ed35cf7e81dc551434365eee2cb593ddbf
manyb2ns <manyb2ns@gmail.com>
2023-10-24 22:36:41 +0900

    update: start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 aws pull $ECR_URL
 
-docker run -it -p $PORT:$PORT $URL
+docker run -it -p $PORT:$PORT $ECR_URL
