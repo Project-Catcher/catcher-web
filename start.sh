a814d641134af8144e310d700c754383b7cf256d
manyb2ns <manyb2ns@gmail.com>
2023-10-24 22:52:55 +0900

    udpate: start.sh

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ echo $TEST
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker pull $ECR_URL
 
-docker run -it -p $PORT:$PORT $ECR_URL
+docker run -i -p $PORT:$PORT $ECR_URL
