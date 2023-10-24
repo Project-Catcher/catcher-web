0deade3dd7c8ce6e50d7eb1d112501b94a751ce0
manyb2ns <manyb2ns@gmail.com>
2023-10-25 01:03:36 +0900

    update: front.yaml, start.sh for CodeDeploy test

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-echo """
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-docker pull $ECR_URL
-
-docker run -it -p $PORT:$PORT $ECR_URL
-"""
-
 source /home/ec2-user/env.sh
-echo $TEST
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker pull $ECR_URL
 
-docker run -i -p $PORT:$PORT $ECR_URL
+docker create -i --name front -p $PORT:$PORT $ECR_URL
