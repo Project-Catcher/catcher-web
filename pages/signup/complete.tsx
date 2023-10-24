6a816d05e8507b96f80fb9cb1d79a54191164162
jerry <becomegoodguy@gmail.com>
2023-10-24 15:25:55 +0900

    feat: 회원가입 완료 페이지

--- pages/signup/complete.tsx
+++ pages/signup/complete.tsx
@@ -1,8 +1,23 @@
+// 회원가입 - 회원가입 완료 페이지
+
 import React from "react";
-import Background from "../../src/signup/Background";
+
+import { Background, Complete, Greeting } from "signup";
 
 const complete = () => {
-  return <Background>complete</Background>;
+  return (
+    <Background>
+      <div className="flex justify-center gap-4 pt-[100px]">
+        <Greeting
+          title="만나서 반갑습니다"
+          content="캐쳐와 함께라면 하루를 알차게 채우는 건 어렵지 않아요"
+        />
+        <div className="pt-[150px]">
+          <Complete />
+        </div>
+      </div>
+    </Background>
+  );
 };
 
 export default complete;
