c9e630228b3c8de21ab855c326f35b5251f271d5
manyb2ns <manyb2ns@gmail.com>
2023-10-25 17:41:18 +0900

    update: front.yml, start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ source /home/ec2-user/env.sh
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker pull $ECR_URL
 
+docker rm -f front
 docker create -i --name front -p $PORT:$PORT $ECR_URL
