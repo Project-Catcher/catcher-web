a7296b20d8231917161ec00f485692130db3a4b8
manyb2ns <manyb2ns@gmail.com>
2023-10-26 00:29:00 +0900

    update: 신규 이미지 저장 시 기존 이미지 삭제 (ECR, Front Server Local)

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# 환경변수 초기화
 source /home/ec2-user/env.sh
 
+# ECR 로그인 후 신규 이미지 PULL
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker pull $ECR_URL
 
+# 기존 컨테이너 삭제 후 신규 컨테이너 생성
 docker rm -f front
 docker create -i --name front -p $PORT:$PORT $ECR_URL
+docker start front
+
+# 기존 이미지 삭제
+docker image rm $(docker images | egrep -v "SIZE|$TAG" | awk -F ' ' '{print $3}')
