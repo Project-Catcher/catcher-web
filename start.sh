c14ea5375cfc9f22cc68424f9ac3cdd46cb5f718
manyb2ns <manyb2ns@gmail.com>
2023-10-26 23:04:17 +0900

    update: start.sh 변수명 오타 수정

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ docker pull $FRONT_ECR_URL
 echo '기존 컨테이너 삭제 후 신규 컨테이너 생성'
 docker ps -a
 
-check_cont=$(docker ps -a | grep front)
-if [ $(echo $check_cont | awk '{print length($0)}') != 0 ]; then
+CHECK_CONT=$(docker ps -a | grep front)
+if [ $(echo $CHECK_CONT | awk '{print length($0)}') != 0 ]; then
 docker rm -f front
 fi
 
@@ -21,8 +21,10 @@ docker create -i --name front -p 80:$FRONT_PORT $FRONT_ECR_URL # temp
 docker start front
 
 echo '기존 이미지 삭제'
-check_img=$(docker images | egrep -v "SIZE|$FRONT_TAG" | awk '{print $2}')
-if [ $(echo $check_img | awk '{print length($0)}') != 0 ]; then
-echo "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$DELETE_TAG"
+CHECK_IMG=$(docker images | egrep -v "SIZE|$FRONT_TAG" | awk '{print $2}')
+echo $CHECK_IMG
+
+if [ $(echo $CHECK_IMG | awk '{print length($0)}') != 0 ]; then
+echo "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$CHECK_IMG"
 docker image rm "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$DELETE_TAG"
 fi
