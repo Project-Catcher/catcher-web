7e52501841701e62296be9beea5d79fb9ad48fa3
manyb2ns <manyb2ns@gmail.com>
2023-10-26 17:08:08 +0900

    update: start.sh 변수명 변경

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ source /home/ec2-user/env.sh
 
 # ECR 로그인 후 신규 이미지 PULL
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-docker pull $ECR_URL
+docker pull $FRONT_ECR_URL
 
 # 기존 컨테이너 삭제 후 신규 컨테이너 생성
 docker rm -f front
-docker create -i --name front -p $PORT:$PORT $ECR_URL
+docker create -i --name front -p $FRONT_PORT:$FRONT_PORT $FRONT_ECR_URL
 docker start front
 
 # 기존 이미지 삭제
