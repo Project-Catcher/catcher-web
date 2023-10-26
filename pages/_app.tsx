70c8eb234a13b46cbf9238d238c3cee5da8302a5
jerry <becomegoodguy@gmail.com>
2023-10-26 18:54:37 +0900

    feat: Header 컴포넌트 개선

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import Alert from "../src/shared/modal/Alert";
+import Header from "@shared/component/header";
 import "@shared/styles/globals.css";
+import Alert from "../src/shared/modal/Alert";
 import Confirm from "../src/shared/modal/Confirm";
-import Header from '@shared/component/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/shared/component/header/index.tsx
+++ src/shared/component/header/index.tsx
@@ -1,18 +1,47 @@
+// 헤더 컴포넌트
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import logo from "../../../../public/header/lovo.svg";
 
-
+// TODO: 로그인 회원일 시, 보이는 탭 다르게 수정하기
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className="fixed top-0 left-0 right-0 p-4 bg-blue-500">
-      <div className="container mx-auto">
-        <div className="flex">
-          <Image
-            src="../../../../header/logo.svg"
-            alt="Catcher Logo"
-            width={72}
-            height={16}
-          />
-          <h1 className="text-2xl font-semibold text-white">내 고정 헤더</h1>
+    // TODO: bg 색상 변경하기 (현재는 흰 배경에 보이지 않아서 임의로 파란색 배경)
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-between px-20 py-4 bg-blue-500">
+      <div className="flex items-center cursor-pointer">
+        <Link href="/">
+          <Image src={logo} alt="Catcher Logo" width={140} height={40} />
+        </Link>
+      </div>
+      <div className="flex items-center gap-12">
+        {tabs.map((tab, i) => (
+          <Link key={`tab-${i}`} href={tab.path}>
+            <div className="text-lg font-medium text-white cursor-pointer hover:underline">
+              {/* TODO: 글자색이 페이지마다 다름 확인 후 수정하기 */}
+              {tab.title}
+            </div>
+          </Link>
+        ))}
+
+        <div className="flex w-[303px] h-[62px] bg-white rounded-[34.50px] shadow p-1 items-center">
+          {loginTabs.map((loginTab, i) => (
+            <Link key={`loginTab-${i}`} href={loginTab.path}>
+              <div
+                className={`w-[145px] h-[49px] ${
+                  router.asPath === loginTab.path
+                    ? "bg-orange-100 hover:bg-orange-200"
+                    : "bg-white hover:bg-gray-200"
+                } rounded-[34.50px] p-1 ml-2 cursor-pointer flex-center`}
+              >
+                <span className="w-[70px] h-[25px] text-center text-gray-500 text-xl font-bold font-['Roboto'] capitalize">
+                  {loginTab.title}
+                </span>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </header>
@@ -20,3 +49,25 @@ const Header = () => {
 };
 
 export default Header;
+
+const tabs = [
+  {
+    title: "일정 찾기",
+    path: "/schedule",
+  },
+  {
+    title: "일정 만들기",
+    path: "/create-schedule",
+  },
+];
+
+const loginTabs = [
+  {
+    title: "Log in",
+    path: "/login",
+  },
+  {
+    title: "sign up",
+    path: "/signup",
+  },
+];
