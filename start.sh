7c94a56a73d51bbd4a72a7ad201b2ebaddef45d8
manyb2ns <manyb2ns@gmail.com>
2023-10-26 23:17:32 +0900

    test

--- start.sh
+++ start.sh
@@ -14,6 +14,7 @@ docker ps -a
 CHECK_CONT=$(docker ps -a | grep front)
 if [ $(echo $CHECK_CONT | awk '{print length($0)}') != 0 ]; then
 docker rm -f front
+echo '기존 컨테이너 삭제 완료'
 fi
 
 # docker create -i --name front -p $FRONT_PORT:$FRONT_PORT $FRONT_ECR_URL
@@ -25,6 +26,6 @@ CHECK_IMG=$(docker images | egrep -v "SIZE|$FRONT_TAG" | awk '{print $2}')
 echo $CHECK_IMG
 
 if [ $(echo $CHECK_IMG | awk '{print length($0)}') != 0 ]; then
-echo "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$CHECK_IMG"
+echo "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$CHECK_IMG 이미지 삭제"
 docker image rm "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$FRONT_ECR_NAME:$CHECK_IMG"
 fi
