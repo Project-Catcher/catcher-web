81006a3be4dce434dfd4992c7dab1f15f8a3732b
최진욱 <jinwook.fe@gmail.com>
2023-10-28 02:24:43 +0900

    [feat] 마이페이지 좌측 메뉴바 구현

---
+++ src/mypage/components/MypageContent.tsx
@@ -0,0 +1,11 @@
+import MenuBar from "./MenuBar";
+
+const MypageContent = () => {
+  return (
+    <div className="mx-auto">
+      <MenuBar />
+    </div>
+  );
+};
+
+export default MypageContent;
