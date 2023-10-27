37f00512e943f2e56a8091e65a6b97e7713cc1b0
jerry <becomegoodguy@gmail.com>
2023-10-28 02:22:28 +0900

    feat: 회원가입 페이지에서 사용되는 공통 input 컴포넌트 추가

---
+++ src/signup/Input.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import React, { InputHTMLAttributes, useState } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  essential?: boolean;
+  fieldName: string;
+  placeholder?: string;
+  type?: string;
+  shape: "semi-round" | "normal";
+  labelStyle?: string;
+  divStyle?: string;
+  inputStyle?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  readOnly?: boolean;
+}
+
+const Input = ({
+  label,
+  type,
+  fieldName,
+  essential = false,
+  placeholder,
+  shape,
+  labelStyle,
+  divStyle,
+  inputStyle,
+  onChange,
+  ...props
+}: InputProps) => {
+  const [preview, setPreview] = useState(type === "password" ? false : true);
+
+  return (
+    <>
+      {label && (
+        <div
+          className={`text-xs font-medium text-[#2C2C2E] mb-[5px] ${labelStyle}"`}
+        >
+          {label}
+          {essential && (
+            <span className="w-[11px] h-[22px] text-red-600 text-xl font-light">
+              *
+            </span>
+          )}
+        </div>
+      )}
+      <div
+        className={`${
+          shape === "semi-round" ? "rounded-[9px] " : ""
+        }flex w-full items-center justify-evenly border border-solid border-[#ADADAD] focus-within:border-[#4285F4] ${divStyle}`}
+      >
+        <input
+          name={fieldName}
+          className={`${inputStyle}`}
+          type={`${preview ? "text" : "password"}`}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e);
+          }}
+          {...(props.readOnly ? { disabled: true } : {})}
+          {...props}
+        />
+
+        {/* type이 password인 경우에만 preview 기능 */}
+        {type === "password" ? (
+          <Image
+            className="cursor-pointer"
+            src="/images/samples/eye.svg"
+            alt="eye"
+            width={22}
+            height={19}
+            onMouseDown={() => setPreview(true)}
+            onMouseUp={() => setPreview(false)}
+          />
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Input;
