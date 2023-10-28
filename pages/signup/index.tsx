2f995035b85cb651c02155f1fdc98e3f0684d33a
jerry <becomegoodguy@gmail.com>
2023-10-29 01:18:46 +0900

    feat: 회원가입 내부 세 번째 입력 페이지 (프로필 정보)

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,10 +1,11 @@
-// 회원가입 - 서비스 약관 동의 페이지
+// 회원가입 페이지
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { Background } from "signup";
 import ConsentToService from "signup/ConsentToService";
 import Signup_1 from "signup/Signup_1";
 import Signup_2 from "signup/Signup_2";
+import Signup_3 from "signup/Signup_3";
 import { signupPageState } from "@shared/recoil/signup";
 
 const Signup = () => {
@@ -16,6 +17,7 @@ const Signup = () => {
         {currentPage === 1 && <ConsentToService />}
         {currentPage === 2 && <Signup_1 />}
         {currentPage === 3 && <Signup_2 />}
+        {currentPage === 4 && <Signup_3 />}
       </div>
     </Background>
   );
