78ba72bdea3e9e960e88975028765cd8b154160d
최진욱 <jinwook.fe@gmail.com>
2023-10-30 16:15:51 +0900

    [design] 기본정보 이름,닉네임,전화번호,이메일 추가

--- src/mypage/components/BasicInfo.tsx
+++ src/mypage/components/BasicInfo.tsx
@@ -9,9 +9,13 @@ const BasicInfo = () => {
       <button className="float-right w-[76px] h-[33px] border border-[#D4D8E5] rounded-[5px] text-[14px] text-[#666666] bg-[#F4F5F8]">
         정보수정
       </button>
-      <div className="w-full mt-[4px] pb-[16px] border-b border-[#DCDCDC]">
-        <div className="w-[60px] h-[60px] text-white bg-black rounded-[50%]">
+      <div className="flex w-full mt-[4px] pb-[16px] border-b border-[#DCDCDC]">
+        <div className="inline-block w-[60px] h-[60px] text-white bg-black rounded-[50%]">
           image
+        </div>
+        <div className="inline-block pl-[13px]">
+          <div className="text-[22px] text-[#333333] font-bold">마승연</div>
+          <div className="text-[14px] text-[#666666] font-medium">명란마요</div>
         </div>
       </div>
       <div className="px-[11px] py-[13px]">
@@ -22,7 +26,7 @@ const BasicInfo = () => {
             width={16}
             height={16}
           />
-          <div className="ml-[18px]">phone</div>
+          <div className="ml-[18px]">+82 10-8***-2***</div>
         </div>
         <div className="flex items-center">
           <Image
@@ -31,7 +35,7 @@ const BasicInfo = () => {
             width={16}
             height={16}
           />
-          <div className="ml-[18px]">email</div>
+          <div className="ml-[18px]">ms*****@naver.com</div>
         </div>
       </div>
     </div>
--- src/mypage/components/BriefInfo.tsx
+++ src/mypage/components/BriefInfo.tsx
@@ -5,7 +5,7 @@ const BriefInfo = () => {
         <div className="w-[143px] h-[143px] text-center text-white bg-black rounded-[50%] mx-auto mb-[7px]">
           image
         </div>
-        <div className="">닉네임</div>
+        <div className="text-[18px] font-bold">닉네임</div>
       </div>
       <div className="w-full h-[50px] rounded-[9px] bg-[#E2FEFC] border border-[#22AFFF] text-[#22AFFF] text-center font-semibold leading-[50px] mb-[35px]">
         인증된 사용자
