9930bad5962858a544bdb1e1b18d78e8d451ff97
최진욱 <jinwook.fe@gmail.com>
2023-11-01 16:40:14 +0900

    [fix] instructions 로직 변경

--- src/shared/components/Instructions.tsx
+++ src/shared/components/Instructions.tsx
@@ -6,8 +6,6 @@ interface InstructionsProps {
 }
 
 const Instructions = ({ type }: InstructionsProps) => {
-  const [isHover, setIsHover] = useState(false);
-
   const instruction =
     type === "phone"
       ? "1588 번호가 스팸 문자로 등록되어 있는 것은 아닌지 확인해주세요.\n 스팸 문자로 등록되어 있지 않다면,\n 다시 한 번 ‘인증번호 받기’를 눌러주세요."
@@ -17,18 +15,14 @@ const Instructions = ({ type }: InstructionsProps) => {
     <>
       <div className="flex items-center relative float-right text-[10px] text-[#8D8D8D]">
         인증번호가 오지 않나요
-        <div
-          className="inline-block w-[22px] h-[22px] bg-question bg-no-repeat cursor-pointer"
-          onMouseOver={() => setIsHover(true)}
-          onMouseOut={() => setIsHover(false)}
-        />
-        {isHover && (
-          <div className="absolute w-[250px] h-[57px] text-[9px] leading-[13px] text-[#777777] -tracking-[.065em] font-medium top-[22px] bg-white border border-[#BCBBBB] shadow-[0_2px_5px_0_rgba(0,0,0,0.15)] pl-[12px] py-[7px] before:absolute before:-top-[9px] before:left-[110px] before:border-b-[8px] before:border-r-[8px] before:border-b-[#BCBBBB] before:border-r-transparent after:absolute after:-top-[6.3px] after:left-[111px] after:border-r-[7px] after:border-b-[7px] after:border-b-[#FFFFFF] after:border-r-transparent">
+        <div className="inline-block group">
+          <div className="w-[22px] h-[22px] bg-question bg-no-repeat cursor-pointer" />
+          <div className="hidden group-hover:inline-block absolute left-[0px] w-[250px] h-[57px] text-[9px] leading-[13px] text-[#777777] -tracking-[.065em] font-medium top-[22px] bg-white border border-[#BCBBBB] shadow-[0_2px_5px_0_rgba(0,0,0,0.15)] pl-[12px] py-[7px] before:absolute before:-top-[9px] before:left-[110px] before:border-b-[8px] before:border-r-[8px] before:border-b-[#BCBBBB] before:border-r-transparent after:absolute after:-top-[6.3px] after:left-[111px] after:border-r-[7px] after:border-b-[7px] after:border-b-[#FFFFFF] after:border-r-transparent">
             {instruction.split("\n").map((line) => (
               <p key={line}>{line}</p>
             ))}
           </div>
-        )}
+        </div>
       </div>
     </>
   );
