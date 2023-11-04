3f939b5d0303e10014dc25d1f054440f852fbb6d
jerry <becomegoodguy@gmail.com>
2023-11-04 22:07:11 +0900

    feat: InputCalendar 컴포넌트 추가

---
+++ src/create/const/index.ts
@@ -0,0 +1,60 @@
+export const defaultCardList = [
+  {
+    theme: "캠핑 레져",
+    img: "/assets/schedule/sample_img.png",
+    title: "단풍구경 관악산 등반 하실 분?",
+    writer: "명란마요",
+    status: true,
+    content: ` 관악산으로 토요일 오전 10시에 등산 일정
+  
+    연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+    방향으로 하산`,
+    durationStart: "2023/10/25",
+    durationEnd: "2023/10/28",
+  },
+  {
+    theme: "캠핑 레져",
+    title: "제목을 입력해주세요",
+    writer: "명란마요",
+    status: false,
+    durationStart: "2023/10/25",
+    durationEnd: "2023/10/28",
+  },
+  {
+    theme: "캠핑 레져",
+    title: "제목을 입력해주세요2",
+    writer: "명란마요",
+    status: false,
+    durationStart: "2023/11/01",
+    durationEnd: "2023/11/02",
+  },
+  {
+    theme: "캠핑 레져",
+    title: "제목을 입력해주세요3",
+    writer: "명란마요",
+    status: false,
+    durationStart: "2023/11/03",
+    durationEnd: "2023/11/03",
+  },
+  {
+    theme: "캠핑 레져",
+    img: "/assets/schedule/sample_img.png",
+    title: "단풍구경 관악산 등반 하실 분?",
+    writer: "명란마요",
+    status: true,
+    content: ` 관악산으로 토요일 오전 10시에 등산 일정
+  
+    연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+    방향으로 하산`,
+    durationStart: "2023/11/03",
+    durationEnd: "2023/11/04",
+  },
+  {
+    theme: "캠핑 레져",
+    title: "제목을 입력해주세요3",
+    writer: "명란마요",
+    status: false,
+    durationStart: "2023/11/04",
+    durationEnd: "2023/11/06",
+  },
+];
