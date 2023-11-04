77fb2dd5fa33e9a6c29651c111a5e5ddb24f4d06
jerry <becomegoodguy@gmail.com>
2023-11-04 22:40:00 +0900

    feat: Tab 명칭을 ScheduleTab으로 변경

---
+++ src/create/components/ScheduleTab.tsx
@@ -0,0 +1,42 @@
+// 일정 탭 컴포넌트
+import React from "react";
+import { TabType } from "./Create";
+
+interface ScheduleTab {
+  currentTab: string;
+  onClickTab: (tab: TabType) => void;
+}
+
+const ScheduleTab = ({ currentTab, onClickTab }: ScheduleTab) => {
+  return (
+    <>
+      <div className="text-zinc-800 text-[32px] font-bold capitalize">
+        모든 일정
+      </div>
+      <div className="h-[30px] relative mt-9 flex gap-x-10">
+        {tabItems.map((tab, i) => (
+          <div
+            key={`schedule Tab-${i}`}
+            className={`cursor-pointer ${
+              currentTab === tab.title
+                ? "border-b-2 border-pink-400 text-zinc-800 font-bold"
+                : "text-zinc-400 font-medium"
+            }`}
+            onClick={() => onClickTab(tab.title as TabType)}
+          >
+            {tab.title}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ScheduleTab;
+
+const tabItems = [
+  { title: "전체" },
+  { title: "진행 중인 일정" },
+  { title: "완료된 일정" },
+  { title: "임시 저장" },
+];
