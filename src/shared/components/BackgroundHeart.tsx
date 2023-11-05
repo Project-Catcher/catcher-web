07ce6c5a854e1fb7e89efb64ea5f2e2217f44d16
최진욱 <jinwook.fe@gmail.com>
2023-11-05 16:12:36 +0900

    [design] z-index 추가 및 애니메이션 속도 조정

--- src/shared/components/BackgroundHeart.tsx
+++ src/shared/components/BackgroundHeart.tsx
@@ -4,7 +4,7 @@ interface HeartAnimation {
   src: string;
   width: number;
   height: number;
-  position: string;
+  style: string;
 }
 
 const BackgroundHeart = () => {
@@ -13,47 +13,44 @@ const BackgroundHeart = () => {
       src: "/images/samples/heart/heart_xs.svg",
       width: 25,
       height: 25,
-      position: "top-[0px] left-[0px]",
+      style: "top-[0px] left-[0px] animated-balloon-3",
     },
     {
       src: "/images/samples/heart/heart_xl.svg",
       width: 85,
       height: 76,
-      position: "top-[150px] left-[70px]",
+      style: "top-[150px] left-[70px] animated-balloon-7",
     },
     {
       src: "/images/samples/heart/heart_md.svg",
       width: 51,
       height: 46,
-      position: "top-[550px] left-[100px]",
+      style: "top-[550px] left-[100px] animated-balloon-5",
     },
     {
       src: "/images/samples/heart/heart_sm.svg",
       width: 33,
       height: 30,
-      position: "top-[123px] left-[812px]",
+      style: "top-[123px] left-[812px] animated-balloon-4",
     },
     {
       src: "/images/samples/heart/heart_lg.svg",
       width: 68,
       height: 61,
-      position: "top-[530px] left-[872px]",
+      style: "top-[530px] left-[872px] animated-balloon-6",
     },
     {
       src: "/images/samples/heart/heart_2xs.svg",
       width: 17,
       height: 15,
-      position: "left-[1000px]",
+      style: "left-[1000px] animated-balloon-3",
     },
   ];
 
   return (
-    <div className="absolute w-[1225px] top-[234px] left-[50%] -translate-x-[50%]">
-      {hearts.map(({ src, width, height, position }) => (
-        <div
-          key={src}
-          className={`inline-block relative animated-balloon ${position}`}
-        >
+    <div className="absolute w-[1225px] top-[234px] left-[50%] -translate-x-[50%] -z-10">
+      {hearts.map(({ src, width, height, style }) => (
+        <div key={src} className={`inline-block relative ${style}`}>
           <Image src={src} alt={src} width={width} height={height} />
         </div>
       ))}
