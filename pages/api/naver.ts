015e4f1643c392851729feba6da6c5c593b853d6
최진욱 <jinwook.fe@gmail.com>
2023-11-10 16:03:50 +0900

    [feat] axios 적용

--- pages/api/naver.ts
+++ pages/api/naver.ts
@@ -1,36 +1,36 @@
+import axios from "axios";
 import { setCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method, query } = req;
   const code = query.code;
 
   if (method === "GET" && code) {
-    // TODO: axios, 백엔드 api 연결
+    // TODO: 백엔드 api 연결
     try {
-      fetch(
+      const response = await axios.post(
         `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_REST_API_KEY_NAVER}&client_secret=${process.env.NEXT_PUBLIC_CLIENT_SECRET_NAVER}&code=${code}`,
+        null,
         {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
         },
-      )
-        .then((_res) => {
-          return _res.json();
-        })
-        .then((data) => {
-          const max_age = 3600000; // TODO: 시간 설정
-          // TODO: cookie name
-          setCookie("access_token_naver", data.access_token, {
-            req,
-            res,
-            maxAge: max_age,
-          });
-          res.redirect(302, "/mypage"); // TODO: 로그인 요청한 페이지로
+      );
+
+      if (response.status === 200) {
+        const data = response.data;
+        const MAX_AGE = 1000 * 60 * 60; // TODO: 시간 설정
+        // TODO: cookie name
+        setCookie("access_token_naver", data.access_token, {
+          // TODO: httponly, secure, expires
+          req,
+          res,
+          maxAge: MAX_AGE,
+          path: "/",
         });
+        res.redirect(302, "/login"); // TODO: 로그인 요청한 페이지로
+      }
     } catch (error) {
       console.log(error);
     }
