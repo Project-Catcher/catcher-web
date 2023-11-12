36fbb80f367e116ec108983693f93b773beb8a1c
최진욱 <jinwook.fe@gmail.com>
2023-11-12 14:34:56 +0900

    [chore] 안내 문구 constant 분리

---
+++ src/makeplan/constants/index.ts
@@ -0,0 +1,13 @@
+export const TITLE = {
+  remains: "잠깐, 작성중이던 일정이 있어요",
+  nthPlan: (number: number) => `명란마요 님의 ${number}번째 일정`,
+  tag: "태그 및 일정 템플릿",
+  fill: "일정 채우기",
+  finish: "작성 마무리",
+};
+
+export const SUBTITLE = {
+  fighting: (nickname: string) => `${nickname}님만의 일정을 완성해보세요`,
+  withyou: "일정이 성공할 수 있도록 캐쳐가 함께할게요.",
+  fillyourplan: (nickname: string) => `${nickname} 님의 일정을 채워주세요!`,
+};
