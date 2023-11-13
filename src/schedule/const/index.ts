a7dae54778eeb9c05e7550a3e86ad0a851c7651c
jerry <becomegoodguy@gmail.com>
2023-11-13 22:43:27 +0900

    feat: 테스트 데이터 추가

--- src/schedule/const/index.ts
+++ src/schedule/const/index.ts
@@ -2,7 +2,7 @@ export const defaultCardList = [
   {
     theme: "캠핑 레져",
     img: "/assets/schedule/sample_img.png",
-    title: "단풍구경 관악산 등반 하실 분?",
+    title: "1단풍구경 관악산 등반 하실 분?",
     content: ` 관악산으로 토요일 오전 10시에 등산 일정
   
     연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
@@ -10,11 +10,137 @@ export const defaultCardList = [
     writer: "명란마요",
     status: true,
     location: "관악구",
-    durationStart: "2023/11/07",
-    durationEnd: "2023/1",
+    durationStart: "2023/11/14",
+    durationEnd: "2023/11/15",
     createdAt: "2023/10/24",
     like: 13,
     comment: 5,
     marked: 7,
   },
+  {
+    theme: "캠핑 레져",
+    img: "/assets/schedule/sample_img.png",
+    title: "2단풍구경 관악산 등반 하실 분? 말이 길어지면 줄임",
+    content: ` 관악산으로 토요일 오전 10시에 등산 일정
+  
+    연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+    방향으로 하산`,
+    writer: "명란마요",
+    status: true,
+    location: "관악구",
+    durationStart: "2023/11/13",
+    durationEnd: "2023/11/14",
+    createdAt: "2023/10/24",
+    like: 13,
+    comment: 5,
+    marked: 7,
+  },
+  {
+    theme: "캠핑 레져",
+    img: "/assets/schedule/sample_img.png",
+    title: "3단풍구경 관악산 등반 하실 분?",
+    content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+    연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+    방향으로 하산`,
+    writer: "명란마요",
+    status: true,
+    location: "관악구",
+    durationStart: "2023/11/13",
+    durationEnd: "2023/11/14",
+    createdAt: "2023/10/24",
+    like: 13,
+    comment: 5,
+    marked: 7,
+  },
+  // {
+  //   theme: "캠핑 레져",
+  //   img: "/assets/schedule/sample_img.png",
+  //   title: "4단풍구경 관악산 등반 하실 분?",
+  //   content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+  //   연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+  //   방향으로 하산`,
+  //   writer: "명란마요",
+  //   status: true,
+  //   location: "관악구",
+  //   durationStart: "2023/11/13",
+  //   durationEnd: "2023/11/14",
+  //   createdAt: "2023/10/24",
+  //   like: 13,
+  //   comment: 5,
+  //   marked: 7,
+  // },
+  // {
+  //   theme: "캠핑 레져",
+  //   img: "/assets/schedule/sample_img.png",
+  //   title: "5단풍구경 관악산 등반 하실 분?",
+  //   content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+  //   연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+  //   방향으로 하산`,
+  //   writer: "명란마요",
+  //   status: true,
+  //   location: "관악구",
+  //   durationStart: "2023/11/13",
+  //   durationEnd: "2023/11/14",
+  //   createdAt: "2023/10/24",
+  //   like: 13,
+  //   comment: 5,
+  //   marked: 7,
+  // },
+  // {
+  //   theme: "캠핑 레져",
+  //   img: "/assets/schedule/sample_img.png",
+  //   title: "6단풍구경 관악산 등반 하실 분?",
+  //   content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+  //   연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+  //   방향으로 하산`,
+  //   writer: "명란마요",
+  //   status: true,
+  //   location: "관악구",
+  //   durationStart: "2023/11/13",
+  //   durationEnd: "2023/11/14",
+  //   createdAt: "2023/10/24",
+  //   like: 13,
+  //   comment: 5,
+  //   marked: 7,
+  // },
+  // {
+  //   theme: "캠핑 레져",
+  //   img: "/assets/schedule/sample_img.png",
+  //   title: "7단풍구경 관악산 등반 하실 분?",
+  //   content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+  //   연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+  //   방향으로 하산`,
+  //   writer: "명란마요",
+  //   status: true,
+  //   location: "관악구",
+  //   durationStart: "2023/11/13",
+  //   durationEnd: "2023/11/14",
+  //   createdAt: "2023/10/24",
+  //   like: 13,
+  //   comment: 5,
+  //   marked: 7,
+  // },
+  // {
+  //   theme: "캠핑 레져",
+  //   img: "/assets/schedule/sample_img.png",
+  //   title: "8단풍구경 관악산 등반 하실 분?",
+  //   content: ` 관악산으로 토요일 오전 10시에 등산 일정
+
+  //   연주대 12시 도착 및 12시부터 30분간 사진촬영 예정 / 2시에 서울대
+  //   방향으로 하산`,
+  //   writer: "명란마요",
+  //   status: true,
+  //   location: "관악구",
+  //   durationStart: "2023/11/13",
+  //   durationEnd: "2023/11/14",
+  //   createdAt: "2023/10/24",
+  //   like: 13,
+  //   comment: 5,
+  //   marked: 7,
+  // },
 ];
