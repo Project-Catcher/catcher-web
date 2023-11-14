a9d78c711a1b9658bf9ee1a62f2c3d43c717c5dd
jerry <becomegoodguy@gmail.com>
2023-11-14 21:03:15 +0900

    feat: ContentFilter 컴포넌트 추가

--- src/schedule/components/All.tsx
+++ src/schedule/components/All.tsx
@@ -2,12 +2,12 @@
 import { defaultCardList } from "@schedule/const";
 import React, { useEffect, useState } from "react";
 import AllContent, { CardItemType } from "./AllContent";
-import InputCalender from "./InputCalender";
+import ContentFilter from "./ContentFilter";
 import ScheduleHeader from "./ScheduleHeader";
 import ScheduleTab from "./ScheduleTab";
 
 type TabType = "전체" | "진행 예정" | "진행중/완료 일정";
-interface DateProps {
+export interface DateProps {
   start: Date | undefined;
   end: Date | undefined;
 }
@@ -109,45 +109,20 @@ const All = () => {
         </div>
       </div>
 
-      {/* 일정 */}
       <div className="flex flex-col items-center min-h-[660px] bg-slate-100 border-t">
-        {/* 필터링 */}
-        <div className="flex items-center justify-center w-full py-3 bg-white">
-          <div className="mr-2 text-sm font-light text-zinc-800">일정 제목</div>
-          <div className="w-[280px] h-[38px] bg-white rounded-[5px] border border-neutral-200 flex items-center mr-6">
-            <input
-              className="w-full px-2 py-1 text-sm font-normal text-zinc-500 focus:outline-none"
-              placeholder="일정 제목을 입력해주세요"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="ml-6 mr-2 text-sm font-light text-zinc-800">
-            모집 기간
-          </div>
-          <InputCalender
-            date={date.start}
-            visible={showCalendar.start}
-            placeholder={"모집 시작일"}
-            handleCalendarClick={() => handleCalendarClick("start")}
-            handleDateChange={handleStartDateChange}
-          />
-          <div className="px-2">-</div>
-          <InputCalender
-            date={date.end}
-            visible={showCalendar.end}
-            placeholder={"모집 종료일"}
-            handleCalendarClick={() => handleCalendarClick("end")}
-            handleDateChange={handleEndDateChange}
-          />
-          <button
-            className="ml-3 w-[90px] h-10 bg-pink-400 rounded-[5px] text-white text-sm font-bold"
-            onClick={onClickSearch}
-          >
-            찾기
-          </button>
-        </div>
-
+        {/* 일정 필터*/}
+        <ContentFilter
+          title={title}
+          setTitle={setTitle}
+          date={date}
+          setDate={setDate}
+          showCalendar={showCalendar}
+          handleCalendarClick={handleCalendarClick}
+          handleStartDateChange={handleStartDateChange}
+          handleEndDateChange={handleEndDateChange}
+          onClickSearch={onClickSearch}
+        />
+        {/* 일정 카드*/}
         <AllContent cardList={cardList} setCardList={setCardList} />
       </div>
     </>
