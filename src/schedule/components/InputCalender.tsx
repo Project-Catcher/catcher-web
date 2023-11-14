89bc7072122d12cdaae17039fc895f82ae959826
jerry <becomegoodguy@gmail.com>
2023-11-14 20:57:42 +0900

    fix: calendar 닫히지 않는 오류 수정

--- src/schedule/components/InputCalender.tsx
+++ src/schedule/components/InputCalender.tsx
@@ -18,23 +18,22 @@ const InputCalender = ({
   handleDateChange,
 }: InputCalenderProps) => {
   return (
-    <div
-      className="w-[145px] h-[38px] bg-white rounded-[5px] border border-neutral-200 flex items-center relative cursor-pointer"
-      onClick={handleCalendarClick}
-    >
-      <input
-        className="w-full px-2 py-1 text-sm font-normal cursor-pointer text-zinc-500 focus:outline-none"
-        placeholder={placeholder}
-        value={date?.toLocaleDateString()}
-        readOnly
-      />
-      <div className="pr-2">
-        <Image
-          src="/assets/schedule/calender.svg"
-          alt="calender"
-          width={13}
-          height={13}
+    <div className="w-[145px] h-[38px] bg-white rounded-[5px] border border-neutral-200  relative cursor-pointer">
+      <div className="h-full flex-center" onClick={handleCalendarClick}>
+        <input
+          className="w-full px-2 py-1 text-sm font-normal cursor-pointer text-zinc-500 focus:outline-none"
+          placeholder={placeholder}
+          defaultValue={date?.toLocaleDateString()}
+          readOnly
         />
+        <div className="pr-2">
+          <Image
+            src="/assets/schedule/calender.svg"
+            alt="calender"
+            width={13}
+            height={13}
+          />
+        </div>
       </div>
       {visible && (
         <div className="absolute top-[38px] left-0 z-30">
