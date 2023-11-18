ec022c62ff78ad924b49a1df506be8d9d1ab4c63
최진욱 <jinwook.fe@gmail.com>
2023-11-18 22:32:59 +0900

    [feat] 아이템 드래그앤드랍 가능여부 및 드래그 함수 추가

--- src/create-schedule/components/CategoryFrame.tsx
+++ src/create-schedule/components/CategoryFrame.tsx
@@ -1,36 +1,57 @@
 import Image from "next/image";
 import React from "react";
-import { CategoryItem } from "./DragnDropContainer";
+import { useSetRecoilState } from "recoil";
+import { selectedScheduleItem } from "@shared/recoil";
+import { CategoryItem } from "@shared/types";
 
 interface CategoryFrameProps {
+  hasPointer?: boolean;
+  itemHeight?: number;
   category: CategoryItem[];
-  onDragStart: (index: number) => void;
 }
 
-const CategoryFrame = ({ category, onDragStart }: CategoryFrameProps) => {
+const CategoryFrame = ({
+  hasPointer,
+  itemHeight = 21,
+  category,
+}: CategoryFrameProps) => {
+  const setSelectedItem = useSetRecoilState(selectedScheduleItem);
+
+  const onDragStart = (index: number) => {
+    setSelectedItem({
+      ...category[index],
+      selectedTime: 0,
+    });
+  };
+
   return (
     <>
       {category.map((_category, index) => (
         <div
-          draggable
-          onDragStart={() => onDragStart(index)}
+          draggable={hasPointer}
+          onDragStart={() => onDragStart!(index)}
           onDragOver={(e) => e.preventDefault()}
-          className={`${
-            index === category.length - 1 ? "" : "mb-[8px]"
-          } flex h-[21px] cursor-pointer`}
+          className={`${index === category.length - 1 ? "" : "mb-[8px] "}${
+            hasPointer ? "cursor-pointer " : ""
+          }flex`}
           key={_category.city + _category.tagBackground}
+          style={{ height: `${itemHeight}px` }}
         >
           <div
             className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[4px_0_0_4px]`}
           >
             <div
-              className={`${_category.tagBackground} brightness-125 w-[2px] h-[13px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
+              style={{
+                height: itemHeight ? `${itemHeight - 8}px` : "13px",
+              }}
+              className={`${_category.tagBackground} brightness-125 w-[2px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
             />
           </div>
           <div
             className={`${_category.tagBackground} flex items-center w-[234px] p-[3px]`}
           >
             <Image
+              draggable={false}
               src="/images/samples/palette.svg"
               alt="palette"
               width={16}
@@ -50,6 +71,9 @@ const CategoryFrame = ({ category, onDragStart }: CategoryFrameProps) => {
             className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[0_4px_4px_0]  `}
           >
             <div
+              style={{
+                height: itemHeight ? `${Number(itemHeight) - 8}px` : "13px",
+              }}
               className={`${_category.tagBackground} brightness-125 w-[2px] h-[13px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
             />
           </div>
