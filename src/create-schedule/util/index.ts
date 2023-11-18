92b160ab6f4cb54110f54430b3370d972960c9fa
최진욱 <jinwook.fe@gmail.com>
2023-11-18 22:58:51 +0900

    [feat] 유틸함수 추가

--- src/create-schedule/util/index.ts
+++ src/create-schedule/util/index.ts
@@ -1,7 +1,7 @@
 import { TableArrayType } from "@create-schedule/components/TimeTable";
 import { PIXEL, TIME } from "@create-schedule/constants";
 import { Dispatch, SetStateAction } from "react";
-import { AppliedItem } from "@shared/types";
+import { AppliedItem, Start2EndTime } from "@shared/types";
 
 export const calculateItemHeight = (
   start: { hour: number; minute: number },
@@ -97,4 +97,73 @@ export const calculateTableHeight = ({
     });
     setTableArray(newTableArray);
   }
+};
+
+export const handleTimeFormat = (time: Start2EndTime) => {
+  const startHour = parseInt(time.start.hour, 10);
+  const startMin = parseInt(time.start.minute, 10);
+  const endHour = parseInt(time.end.hour, 10);
+  const endMin = parseInt(time.end.minute, 10);
+
+  return { startHour, startMin, endHour, endMin };
+};
+
+interface HandleDurationProps {
+  time: Start2EndTime;
+  setDuration: Dispatch<SetStateAction<number>>;
+}
+
+export const handleDuration = ({ time, setDuration }: HandleDurationProps) => {
+  const { startHour, startMin, endHour, endMin } = handleTimeFormat(time);
+  if (endMin < startMin) {
+    const hour = endHour - startHour - 1;
+    const min = 60 + endMin - startMin;
+    setDuration(hour + min / 60);
+
+    return;
+  }
+
+  const hour = endHour - startHour;
+  const min = endMin - startMin;
+  setDuration(hour + min / 60);
+};
+
+export const checkDuplication = (
+  startHour: number,
+  startMin: number,
+  endHour: number,
+  endMin: number,
+  appliedItem: AppliedItem[] | null,
+) => {
+  // 이미 존재하는 테이블 아이템에 대해 검사
+  if (appliedItem) {
+    const isDuplicated = appliedItem.map((item) => {
+      const startRange = item.startTime.hour * 60 + item.startTime.minute;
+      const endRange = item.endTime.hour * 60 + item.endTime.minute;
+      const newItemStartRange = startHour * 60 + startMin;
+      const newItemEndRange = endHour * 60 + endMin;
+
+      if (startRange <= newItemStartRange && endRange > newItemStartRange) {
+        return true;
+      } else if (newItemEndRange > startRange && newItemEndRange <= endRange) {
+        return true;
+      } else if (startRange > newItemStartRange && endRange < newItemEndRange) {
+        return true;
+      }
+      return false;
+    });
+
+    return isDuplicated;
+  }
+  // 테이블이 비었을 경우
+  return [false];
+};
+
+export const sortByStartTime = (appliedItem: AppliedItem[]) => {
+  appliedItem.sort((a, b) => {
+    if (a.startTime.hour !== b.startTime.hour) {
+      return a.startTime.hour - b.startTime.hour;
+    }
+    return a.startTime.minute - b.startTime.minute;
+  });
 };
