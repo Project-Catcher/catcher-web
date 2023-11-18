986962e73945d0f9d2d23cb35821542199c38849
최진욱 <jinwook.fe@gmail.com>
2023-11-18 22:37:00 +0900

    [feat] 타임테이블 넓이 고정

--- src/create-schedule/components/TimeTableContainer.tsx
+++ src/create-schedule/components/TimeTableContainer.tsx
@@ -1,22 +1,19 @@
-import { DragEvent } from "react";
 import CurrentDate from "./CurrentDate";
 import DateMoveButton from "./DateMoveButton";
 import ScheduleTitle from "./ScheduleTitle";
 import TimeTable from "./TimeTable";
 
-interface TimeTableContainerProps {
-  onDrop: (e: DragEvent<HTMLTableRowElement>, index: number) => void;
-}
+interface TimeTableContainerProps {}
 
-const TimeTableContainer = ({ onDrop }: TimeTableContainerProps) => {
+const TimeTableContainer = ({}: TimeTableContainerProps) => {
   return (
     <>
-      <div className="w-[362px]">
+      <div className="min-w-[362px] max-w-[362px]">
         <DateMoveButton />
         <ScheduleTitle title="일정표" />
         <CurrentDate />
         <div className="w-full border border-[#ACBEFF] rounded-[5px] bg-[#FFF9FC]">
-          <TimeTable onDrop={onDrop} />
+          <TimeTable />
         </div>
       </div>
     </>
