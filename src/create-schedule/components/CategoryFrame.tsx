db0cc0876a2b9102702671b4ea11055e5d04cdf9
최진욱 <jinwook.fe@gmail.com>
2023-11-20 20:08:14 +0900

    [feat] 카테고리에 따른 imageSrc 설정

--- src/create-schedule/components/CategoryFrame.tsx
+++ src/create-schedule/components/CategoryFrame.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 import React from "react";
 import { useSetRecoilState } from "recoil";
 import { selectedScheduleItem } from "@shared/recoil";
-import { CategoryItem } from "@shared/types";
+import { CategoryItem, CategoryTags } from "@shared/types";
 
 interface CategoryFrameProps {
   hasPointer?: boolean;
   itemHeight?: number;
+  clickedCategory: CategoryTags;
   category: CategoryItem[];
 }
 
 const CategoryFrame = ({
   hasPointer,
   itemHeight = 21,
+  clickedCategory,
   category,
 }: CategoryFrameProps) => {
   const setSelectedItem = useSetRecoilState(selectedScheduleItem);
@@ -26,59 +28,62 @@ const CategoryFrame = ({
 
   return (
     <>
-      {category.map((_category, index) => (
-        <div
-          draggable={hasPointer}
-          onDragStart={() => onDragStart!(index)}
-          onDragOver={(e) => e.preventDefault()}
-          className={`${index === category.length - 1 ? "" : "mb-[8px] "}${
-            hasPointer ? "cursor-pointer " : ""
-          }flex`}
-          key={_category.city + _category.tagBackground}
-          style={{ height: `${itemHeight}px` }}
-        >
-          <div
-            className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[4px_0_0_4px]`}
-          >
+      {category.map(
+        (_category, index) =>
+          clickedCategory === _category.category && (
             <div
-              style={{
-                height: itemHeight ? `${itemHeight - 8}px` : "13px",
-              }}
-              className={`${_category.tagBackground} brightness-125 w-[2px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
-            />
-          </div>
-          <div
-            className={`${_category.tagBackground} flex items-center w-[234px] p-[3px]`}
-          >
-            <Image
-              draggable={false}
-              src="/images/samples/palette.svg"
-              alt="palette"
-              width={16}
-              height={15}
-            />
-            <span className="text-[10px] text-[#454545] font-semibold ml-[4px] mr-[10px]">
-              {_category.category}
-            </span>
-            <span className="text-[10px] text-[#454545] font-medium grow">
-              {_category.title}
-            </span>
-            <span className="text-[11px] text-[#00000080] font-semibold">
-              {_category.city}
-            </span>
-          </div>
-          <div
-            className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[0_4px_4px_0]  `}
-          >
-            <div
-              style={{
-                height: itemHeight ? `${Number(itemHeight) - 8}px` : "13px",
-              }}
-              className={`${_category.tagBackground} brightness-125 w-[2px] h-[13px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
-            />
-          </div>
-        </div>
-      ))}
+              draggable={hasPointer}
+              onDragStart={() => onDragStart!(index)}
+              onDragOver={(e) => e.preventDefault()}
+              className={`${index === category.length - 1 ? "" : "mb-[8px] "}${
+                hasPointer ? "cursor-pointer " : ""
+              }flex`}
+              key={_category.city + _category.tagBackground}
+              style={{ height: `${itemHeight}px` }}
+            >
+              <div
+                className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[4px_0_0_4px]`}
+              >
+                <div
+                  style={{
+                    height: itemHeight ? `${itemHeight - 8}px` : "13px",
+                  }}
+                  className={`${_category.tagBackground} brightness-125 w-[2px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
+                />
+              </div>
+              <div
+                className={`${_category.tagBackground} flex items-center w-[234px] p-[3px]`}
+              >
+                <Image
+                  draggable={false}
+                  src={_category.imageSrc}
+                  alt={_category.imageSrc}
+                  width={16}
+                  height={15}
+                />
+                <span className="text-[10px] text-[#454545] font-semibold ml-[4px] mr-[10px]">
+                  {_category.category}
+                </span>
+                <span className="text-[10px] text-[#454545] font-medium grow">
+                  {_category.title}
+                </span>
+                <span className="text-[11px] text-[#00000080] font-semibold">
+                  {_category.city}
+                </span>
+              </div>
+              <div
+                className={`${_category.tagBackground} brightness-90 inline-block w-[8px] rounded-[0_4px_4px_0]  `}
+              >
+                <div
+                  style={{
+                    height: itemHeight ? `${Number(itemHeight) - 8}px` : "13px",
+                  }}
+                  className={`${_category.tagBackground} brightness-125 w-[2px] h-[13px] rounded-[1px] opacity-[40%] mx-auto my-[4px]`}
+                />
+              </div>
+            </div>
+          ),
+      )}
     </>
   );
 };
--- src/create-schedule/components/DragnDropContainer.tsx
+++ src/create-schedule/components/DragnDropContainer.tsx
@@ -1,52 +1,96 @@
-import { CategoryItem } from "@shared/types";
+import { useState } from "react";
+import { CategoryItem, CategoryTags } from "@shared/types";
+import Categories from "./Categories";
 import CategoryItems from "./CategoryItems";
-import TimeTableContainer from "./TimeTableContainer";
 
 const DragnDropContainer = () => {
+  const [clickedCategory, setClickedCategory] = useState<CategoryTags>("전체");
   // TODO: fetch category
   const category: CategoryItem[] = [
+    {
+      category: "쇼핑",
+      title: "용산 아이파크몰",
+      city: "용산",
+      tagBackground: "bg-[#A3FAF2]",
+      imageSrc: "/images/samples/category_shopping.svg",
+    },
+    {
+      category: "쇼핑",
+      title: "용산 아이파크몰",
+      city: "용산",
+      tagBackground: "bg-[#FFE779]",
+      imageSrc: "/images/samples/category_shopping.svg",
+    },
+    {
+      category: "쇼핑",
+      title: "용산 아이파크몰",
+      city: "용산",
+      tagBackground: "bg-[#FFC395]",
+      imageSrc: "/images/samples/category_shopping.svg",
+    },
+    {
+      category: "쇼핑",
+      title: "용산 아이파크몰",
+      city: "용산",
+      tagBackground: "bg-[#CFE1FF]",
+      imageSrc: "/images/samples/category_shopping.svg",
+    },
     {
       category: "문화생활",
       title: "마블 영화 감상",
       city: "연남동",
       tagBackground: "bg-[#A3FAF2]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
     {
       category: "문화생활",
       title: "도서관 가서 신간 읽기",
       city: "연남동",
       tagBackground: "bg-[#FFE779]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
     {
       category: "문화생활",
       title: "국립미술관 가서 전시 관람",
       city: "연남동",
       tagBackground: "bg-[#FFC395]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
     {
       category: "문화생활",
       title: "독서모임 참가하기",
       city: "연남동",
       tagBackground: "bg-[#CFE1FF]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
     {
       category: "문화생활",
       title: "예술의 전당에서 뮤지컬 관람",
       city: "연남동",
       tagBackground: "bg-[#DDD1FF]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
     {
       category: "문화생활",
       title: "오일파스텔 그림그리기",
       city: "연남동",
       tagBackground: "bg-[#FFDCDC]",
+      imageSrc: "/images/samples/category_culture.svg",
     },
   ];
 
+  const handleClickCategory = (value: CategoryTags) => {
+    setClickedCategory(value);
+  };
+
   return (
     <>
-      <div className="inline-block px-[50px]">
-        <CategoryItems category={category} />
+      <div className="inline-block h-fit px-[50px]">
+        <Categories
+          clickedCategory={clickedCategory}
+          handleClickCategory={handleClickCategory}
+        />
+        <CategoryItems category={category} clickedCategory={clickedCategory} />
       </div>
     </>
   );
