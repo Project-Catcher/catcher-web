0db45567424ec0586b1a8b58d265b5326378cf88
최진욱 <jinwook.fe@gmail.com>
2023-11-22 14:45:35 +0900

    [feat] 임시 템플릿 데이터 분리

--- src/create-schedule/constants/index.ts
+++ src/create-schedule/constants/index.ts
@@ -32,3 +32,34 @@ export const SCHEDULE_SUBTITLE = {
   date: "준비기간 및 모집기간 등 전체 일정을 고려해 설정해 주세요.",
   city: "일정을 수행할 위치를 선택해주세요.",
 };
+
+export const templateContent = [
+  {
+    id: 1,
+    imageSrc: "image",
+    title: "베이커리 빵지순례",
+    position: "서울시 연남동",
+    requiredTime: "1일 이내",
+  },
+  {
+    id: 2,
+    imageSrc: "image",
+    title: "카페에서 함께 공부해요",
+    position: "서울시 연남동",
+    requiredTime: "2~3일 일정",
+  },
+  {
+    id: 3,
+    imageSrc: "image",
+    title: "베이커리 빵지순례",
+    position: "가로수길",
+    requiredTime: "1일 이내",
+  },
+  {
+    id: 4,
+    imageSrc: "image",
+    title: "호캉스 일정",
+    position: "남해",
+    requiredTime: "2~3일 일정",
+  },
+];
