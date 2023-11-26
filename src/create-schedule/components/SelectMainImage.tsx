bedefe747a6e650450674f90aae6a98fe77839f6
최진욱 <jinwook.fe@gmail.com>
2023-11-26 15:00:03 +0900

    [feat] 썸네일 선택 후 대표 이미지 표시

--- src/create-schedule/components/SelectMainImage.tsx
+++ src/create-schedule/components/SelectMainImage.tsx
@@ -1,19 +1,34 @@
+import axios from "axios";
 import { ThumbnailSelectorProps } from "modalContent/ThumbnailSelector";
 import Image from "next/image";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { useModal } from "@shared/hook";
 import { scheduleAnswers } from "@shared/recoil";
 
 const SelectMainImage = () => {
-  const setImageSrc = useSetRecoilState(scheduleAnswers);
+  const [{ imageSrc }, setImageSrc] = useRecoilState(scheduleAnswers);
   const { openModal } = useModal();
+
+  const getImage = async (id: string) => {
+    try {
+      const image = await axios.get(`https://api.pexels.com/v1/photos/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: process.env.NEXT_PUBLIC_IMAGE_API_KEY,
+        },
+      });
+      setImageSrc((prev) => ({ ...prev, imageSrc: image.data.src.large }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleModal = () => {
     openModal<ThumbnailSelectorProps>({
       contentId: "thumbnailSelector",
       isHeaderCloseBtn: true,
       okCallback: (id: string) => {
-        setImageSrc((prev) => ({ ...prev, imageSrc: id }));
+        getImage(id);
       },
     });
   };
@@ -23,16 +38,19 @@ const SelectMainImage = () => {
       className="w-[222px] h-[126px] border border-[#E0E0E0] rounded-[5px] mr-[8px] cursor-pointer"
       onClick={handleModal}
     >
-      <div className="py-[32.5px] text-center">
+      <div className={`${imageSrc ? "" : "py-[32.5px] text-center"}`}>
         <Image
-          src="/images/samples/camera.svg"
-          alt="camera"
-          width={30}
-          height={30}
+          className="rounded-[4px]"
+          src={`${imageSrc ? imageSrc : "/images/samples/camera.svg"}`}
+          alt="thumbnail"
+          width={imageSrc ? 221 : 30}
+          height={imageSrc ? 125 : 30}
         />
-        <div className="text-[10px] text-[#757575] -tracking-[0.01em]">
-          이미지 선택하기
-        </div>
+        {!imageSrc && (
+          <div className="text-[10px] text-[#757575] -tracking-[0.01em]">
+            이미지 선택하기
+          </div>
+        )}
       </div>
     </div>
   );
