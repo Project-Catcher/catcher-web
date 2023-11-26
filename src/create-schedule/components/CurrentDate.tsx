8118766e29b6ffe3ed470eafb1876ed611ac3b8e
최진욱 <jinwook.fe@gmail.com>
2023-11-26 18:05:18 +0900

    [feat] 타임테이블 요일 표시

--- src/create-schedule/components/CurrentDate.tsx
+++ src/create-schedule/components/CurrentDate.tsx
@@ -1,4 +1,4 @@
-import { handleDateFormat } from "@create-schedule/util";
+import { getDay, handleDateFormat } from "@create-schedule/util";
 
 interface CurrentDateProps {
   currentDate: Date | null;
@@ -7,8 +7,11 @@ interface CurrentDateProps {
 const CurrentDate = ({ currentDate }: CurrentDateProps) => {
   return (
     <div className="w-full h-[32px] bg-[#ACBEFF] rounded-[5px] text-center leading-[32px] mb-[10px]">
-      <p className="text-[18px] text-white font-semibold">
+      <p className="inline-block text-[18px] text-white font-semibold mr-[6px]">
         {currentDate && handleDateFormat(currentDate)}
+      </p>
+      <p className="inline-block text-[18px] text-[#333C57] font-semibold">
+        {currentDate && getDay(currentDate)}
       </p>
     </div>
   );
