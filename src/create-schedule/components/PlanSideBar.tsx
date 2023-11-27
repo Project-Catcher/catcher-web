4e85caf6850db456e5df36e44372ddb76519870d
최진욱 <jinwook.fe@gmail.com>
2023-11-27 21:47:11 +0900

    [fix] 사이드바 버그 수정

--- src/create-schedule/components/PlanSideBar.tsx
+++ src/create-schedule/components/PlanSideBar.tsx
@@ -1,3 +1,8 @@
+import {
+  BASIC_INFO_TAG,
+  FINISH_WRITING_TAG,
+  TAG_N_TEMPLATE_TAG,
+} from "@create-schedule/constants";
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { currentProgress } from "@shared/recoil";
@@ -10,6 +15,13 @@ import TagNTemplate from "./TagNTemplate";
 const PlanSideBar = () => {
   const current = useRecoilValue(currentProgress);
   const [currentTab, setCurrentTab] = useState("일정 제목 입력");
+  console.log(
+    currentTab ===
+      ("일정 제목 입력" ||
+        "대표 이미지 설정" ||
+        "시작일 종료일 설정" ||
+        "장소 선택"),
+  );
 
   const handleTab = (value: string) => {
     setCurrentTab(value);
@@ -18,10 +30,13 @@ const PlanSideBar = () => {
   useEffect(() => {
     switch (current) {
       case 2:
+        if (BASIC_INFO_TAG.includes(currentTab)) return;
         return setCurrentTab("일정 제목 입력");
       case 3:
+        if (TAG_N_TEMPLATE_TAG.includes(currentTab)) return;
         return setCurrentTab("태그");
       case 5:
+        if (FINISH_WRITING_TAG.includes(currentTab)) return;
         return setCurrentTab("공개범위 설정");
       default:
         return setCurrentTab("");
