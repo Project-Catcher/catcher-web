b4faf2b9156f46f017ce49e74a23a752dfd2876e
jerry <becomegoodguy@gmail.com>
2023-12-01 16:13:53 +0900

    feat: msw 설정 및 데이터 받아오기 수정

---
+++ pages/api/mySchedule.ts
@@ -0,0 +1,128 @@
+import axios from "axios";
+
+export const getAllSchedule = async (
+  tab?: string,
+  title?: string,
+  startDate?: Date,
+  endDate?: Date,
+) => {
+  let tabParam;
+
+  if (tab === "진행 예정") {
+    tabParam = "proceed";
+  } else if (tab === "진행중/완료 일정") {
+    tabParam = "ongoing";
+  }
+
+  return await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/getAllSchedule`, {
+    params: {
+      tab: tabParam,
+      title,
+      startDate,
+      endDate,
+    },
+  });
+};
+
+export const getRecruitSchedule = async (
+  tab?: string,
+  title?: string,
+  startDate?: Date,
+  endDate?: Date,
+) => {
+  let tabParam;
+
+  if (tab === "모집 중/예정") {
+    tabParam = "proceed";
+  } else if (tab === "모집 완료") {
+    tabParam = "ongoing";
+  }
+
+  return await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/getRecruitSchedule`,
+    {
+      params: {
+        tab: tabParam,
+        title,
+        startDate,
+        endDate,
+      },
+    },
+  );
+};
+
+export const getParticipateSchedule = async (
+  tab?: string,
+  title?: string,
+  startDate?: Date,
+  endDate?: Date,
+) => {
+  let tabParam = 0;
+
+  if (tab === "승인 대기 중") {
+    tabParam = 1;
+  } else if (tab === "승인 완료") {
+    tabParam = 2;
+  } else if (tab === "승인 거절") {
+    tabParam = 3;
+  }
+
+  return await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/getParticipateSchedule`,
+    {
+      params: {
+        tab: tabParam,
+        title,
+        startDate,
+        endDate,
+      },
+    },
+  );
+};
+
+export const getTemporarySchedule = async (
+  title?: string,
+  startDate?: Date,
+  endDate?: Date,
+) => {
+  return await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/getTemporarySchedule`,
+    {
+      params: {
+        title,
+        startDate,
+        endDate,
+      },
+    },
+  );
+};
+
+export const getMainSchedule = async () => {
+  try {
+    const response = await axios.all([
+      getAllSchedule(),
+      getRecruitSchedule(),
+      getParticipateSchedule(),
+      getTemporarySchedule(),
+    ]);
+
+    // axios.spread를 사용하여 각 요청의 결과를 개별적으로 처리
+    const [
+      allSchedule,
+      recruitSchedule,
+      participateSchedule,
+      temporarySchedule,
+    ] = response;
+
+    // 각 요청의 데이터를 반환합니다.
+    return {
+      allSchedule: allSchedule.data,
+      recruitSchedule: recruitSchedule.data,
+      participateSchedule: participateSchedule.data,
+      temporarySchedule: temporarySchedule.data,
+    };
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    throw error; // 오류를 호출자에게 전달
+  }
+};
---
+++ src/mock/handlers/mySchedule.ts
@@ -0,0 +1,239 @@
+import {
+  allScheduleList,
+  defaultParticipateList,
+  temporaryCardList,
+} from "mock/data/schedule";
+import { http } from "msw";
+
+const baseUrl = "http://localhost:3000/api";
+
+export const myScheduleHandlers = [
+  http.get(`${baseUrl}/getAllSchedule`, ({ request }) => {
+    const currentDate = new Date();
+    const url = new URL(request.url);
+    const tab = url.searchParams.get("tab");
+    const title = url.searchParams.get("title") ?? "";
+    const startDate = url.searchParams.get("startDate");
+    const endDate = url.searchParams.get("endDate");
+
+    let start: Date, end: Date;
+    startDate
+      ? (start = new Date(startDate))
+      : (start = new Date("2019/01/01"));
+    endDate ? (end = new Date(endDate)) : (end = new Date("2025/01/01"));
+
+    if (tab == "proceed") {
+      return new Response(
+        JSON.stringify(
+          allScheduleList.filter((item) => {
+            return (
+              new Date(item.durationStart) > currentDate &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end &&
+              item.title.includes(title)
+            );
+          }),
+        ),
+      );
+    }
+
+    if (tab === "ongoing") {
+      return new Response(
+        JSON.stringify(
+          allScheduleList.filter((item) => {
+            return (
+              new Date(item.durationStart) <= currentDate &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end &&
+              item.title.includes(title)
+            );
+          }),
+        ),
+      );
+    }
+
+    return new Response(
+      JSON.stringify(
+        allScheduleList.filter((item) => {
+          return (
+            new Date(item.durationStart) >= start &&
+            new Date(item.durationEnd) <= end &&
+            item.title.includes(title)
+          );
+        }),
+      ),
+    );
+  }),
+
+  http.get(`${baseUrl}/getRecruitSchedule`, ({ request }) => {
+    const currentDate = new Date();
+    const url = new URL(request.url);
+    const tab = url.searchParams.get("tab");
+    const title = url.searchParams.get("title") ?? "";
+    const startDate = url.searchParams.get("startDate");
+    const endDate = url.searchParams.get("endDate");
+
+    let start: Date, end: Date;
+    startDate
+      ? (start = new Date(startDate))
+      : (start = new Date("2019-01-01"));
+    endDate ? (end = new Date(endDate)) : (end = new Date("2025/01/01"));
+
+    // 모집 중/예정
+    if (tab == "proceed") {
+      return new Response(
+        JSON.stringify(
+          allScheduleList.filter((item) => {
+            return (
+              (new Date(item.recruitStart) > currentDate ||
+                (new Date(item.recruitStart) <= currentDate &&
+                  new Date(item.recruitEnd) >= currentDate &&
+                  item.participateNum < item.participateCapacity)) &&
+              item.title.includes(title) &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end
+            );
+          }),
+        ),
+      );
+    }
+
+    // 모집 완료
+    if (tab === "ongoing") {
+      return new Response(
+        JSON.stringify(
+          allScheduleList.filter((item) => {
+            return (
+              (new Date(item.recruitEnd) < currentDate ||
+                item.participateNum === item.participateCapacity) &&
+              item.title.includes(title) &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end
+            );
+          }),
+        ),
+      );
+    }
+
+    const kk = allScheduleList.filter((item) => {
+      return (
+        new Date(item.durationStart) >= start &&
+        new Date(item.durationEnd) <= end &&
+        item.title.includes(title)
+      );
+    });
+
+    return new Response(
+      JSON.stringify(
+        allScheduleList.filter((item) => {
+          return (
+            new Date(item.durationStart) >= start &&
+            new Date(item.durationEnd) <= end &&
+            item.title.includes(title)
+          );
+        }),
+      ),
+    );
+  }),
+
+  http.get(`${baseUrl}/getParticipateSchedule`, ({ request }) => {
+    const url = new URL(request.url);
+    const tab = url.searchParams.get("tab");
+    const title = url.searchParams.get("title") ?? "";
+    const startDate = url.searchParams.get("startDate");
+    const endDate = url.searchParams.get("endDate");
+
+    let start: Date, end: Date;
+    startDate
+      ? (start = new Date(startDate))
+      : (start = new Date("2019-01-01"));
+    endDate ? (end = new Date(endDate)) : (end = new Date("2025/01/01"));
+
+    // 승인 대기 중
+    if (Number(tab) === 1) {
+      return new Response(
+        JSON.stringify(
+          defaultParticipateList.filter((item) => {
+            return (
+              item.approvalStatus === 0 &&
+              item.title.includes(title) &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end
+            );
+          }),
+        ),
+      );
+    }
+
+    // 승인 완료
+    if (Number(tab) === 2) {
+      return new Response(
+        JSON.stringify(
+          defaultParticipateList.filter((item) => {
+            return (
+              item.approvalStatus === 1 &&
+              item.title.includes(title) &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end
+            );
+          }),
+        ),
+      );
+    }
+
+    // 승인 거절
+    if (Number(tab) === 3) {
+      return new Response(
+        JSON.stringify(
+          defaultParticipateList.filter((item) => {
+            return (
+              item.approvalStatus === 2 &&
+              item.title.includes(title) &&
+              new Date(item.durationStart) >= start &&
+              new Date(item.durationEnd) <= end
+            );
+          }),
+        ),
+      );
+    }
+
+    return new Response(
+      JSON.stringify(
+        defaultParticipateList.filter((item) => {
+          return (
+            item.title.includes(title) &&
+            new Date(item.durationStart) >= start &&
+            new Date(item.durationEnd) <= end
+          );
+        }),
+      ),
+    );
+  }),
+
+  http.get(`${baseUrl}/getTemporarySchedule`, ({ request }) => {
+    const url = new URL(request.url);
+    const title = url.searchParams.get("title") ?? "";
+    const startDate = url.searchParams.get("startDate");
+    const endDate = url.searchParams.get("endDate");
+
+    let start: Date, end: Date;
+    startDate
+      ? (start = new Date(startDate))
+      : (start = new Date("2019/01/01"));
+    endDate ? (end = new Date(endDate)) : (end = new Date("2025/01/01"));
+
+    return new Response(
+      JSON.stringify(
+        temporaryCardList.filter((item) => {
+          return (
+            new Date(item.durationStart ? item.durationStart : "2019/01/01") >=
+              start &&
+            new Date(item.durationEnd ? item.durationEnd : "2015/01/01") <=
+              end &&
+            item.title.includes(title)
+          );
+        }),
+      ),
+    );
+  }),
+];
