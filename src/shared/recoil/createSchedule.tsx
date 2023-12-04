28f349125c5dfa23aa534d121e43d078c4c96ee9
최진욱 <jinwook.fe@gmail.com>
2023-12-04 23:47:15 +0900

    [feat] 사용하지 않는 리코일 제거

--- src/shared/recoil/createSchedule.tsx
+++ src/shared/recoil/createSchedule.tsx
@@ -1,12 +1,7 @@
 import { atom } from "recoil";
-import { CurrentPageType, ScheduleAnswerType } from "@shared/types";
+import { ScheduleAnswerType } from "@shared/types";
 
-export const currentPageName = atom<CurrentPageType>({
-  key: "currentPage",
-  default: "작성 중인 일정",
-});
-
-export const currentProgress = atom<number>({
+export const currentScheduleProgress = atom<number>({
   key: "currentProgress",
   default: 1,
 });
