8fd804a8f29906fdbdb8503cc1422a3e27c81f81
최진욱 <jinwook.fe@gmail.com>
2023-12-08 21:43:56 +0900

    [feat] 기본정보 임시저장 msw 적용

--- src/mock/browser.ts
+++ src/mock/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 import { myScheduleHandlers } from "./handlers/mySchedule";
+import { basicInfoHandlers } from "./handlers/postBasicInfo";
 
-export const worker = setupWorker(...handlers, ...myScheduleHandlers);
+export const worker = setupWorker(
+  ...handlers,
+  ...myScheduleHandlers,
+  ...basicInfoHandlers,
+);
