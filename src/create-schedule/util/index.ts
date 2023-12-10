2edc49013b9d34637e2b1980570ab15a385c4e5f
최진욱 <jinwook.fe@gmail.com>
2023-12-10 18:00:50 +0900

    [feat] 기본정보 버튼 비활성화 추가

--- src/create-schedule/util/index.ts
+++ src/create-schedule/util/index.ts
@@ -3,15 +3,43 @@ import { CREATE_SCHEDULE_PATH } from "@shared/constants";
 import { ScheduleAnswerType } from "@shared/types";
 
 export const postBasicInfo = async (answer: ScheduleAnswerType) => {
-  return await axios.post(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/${CREATE_SCHEDULE_PATH.temporary}`,
-    {
-      title: answer.title,
-      thumbnail: answer.thumbnail,
-      location: answer.location,
-      startAt: answer.startAt,
-      endAt: answer.endAt,
-    },
-    { headers: { "Content-Type": "application/json" } },
-  );
+  if (
+    answer.title &&
+    answer.thumbnail &&
+    answer.startAt &&
+    answer.endAt &&
+    answer.location
+  )
+    return await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/${CREATE_SCHEDULE_PATH.temporary}`,
+      {
+        title: answer.title,
+        thumbnail: answer.thumbnail,
+        location: answer.location,
+        startAt: answer.startAt,
+        endAt: answer.endAt,
+      },
+      { headers: { "Content-Type": "application/json" } },
+    );
+};
+
+export const setButtonDisabled = (
+  callType: "basicInfo" | "template",
+  answer: ScheduleAnswerType,
+) => {
+  if (callType === "basicInfo") {
+    if (
+      answer.title &&
+      answer.thumbnail &&
+      answer.startAt &&
+      answer.endAt &&
+      answer.location
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
 };
