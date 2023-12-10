4a8ef063f539d9310421a1f98920b9716ee93768
최진욱 <jinwook.fe@gmail.com>
2023-12-10 18:06:41 +0900

    [feat] 기본정보 post method try-catch 적용

--- src/create-schedule/util/index.ts
+++ src/create-schedule/util/index.ts
@@ -10,17 +10,23 @@ export const postBasicInfo = async (answer: ScheduleAnswerType) => {
     answer.endAt &&
     answer.location
   )
-    return await axios.post(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/${CREATE_SCHEDULE_PATH.temporary}`,
-      {
-        title: answer.title,
-        thumbnail: answer.thumbnail,
-        location: answer.location,
-        startAt: answer.startAt,
-        endAt: answer.endAt,
-      },
-      { headers: { "Content-Type": "application/json" } },
-    );
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/${CREATE_SCHEDULE_PATH.temporary}`,
+        {
+          title: answer.title,
+          thumbnail: answer.thumbnail,
+          location: answer.location,
+          startAt: answer.startAt,
+          endAt: answer.endAt,
+        },
+        { headers: { "Content-Type": "application/json" } },
+      );
+
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 export const setButtonDisabled = (
