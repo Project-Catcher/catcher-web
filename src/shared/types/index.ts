c1f354d13d89db11085b79134086e08ad912f78d
최진욱 <jinwook.fe@gmail.com>
2023-12-10 17:57:20 +0900

    [feat] 지역 타입 추가

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -95,3 +95,23 @@ export interface TemplateDetail {
   startAt: Date;
   endAt: Date;
 }
+
+export interface LocationType {
+  서울특별시: string[];
+  경기도: string[];
+  인천광역시: string[];
+  강원특별자치도: string[];
+  충청북도: string[];
+  충청남도: string[];
+  대전광역시: string[];
+  세종특별자치시: string[];
+  전라북도: string[];
+  전라남도: string[];
+  광주광역시: string[];
+  경상북도: string[];
+  경상남도: string[];
+  부산광역시: string[];
+  대구광역시: string[];
+  울산광역시: string[];
+  제주특별자치도: string[];
+}
